e25b5beef74a5af30094b2284fcfaa800027a6fa
andrenivisnk <andrenivinsk@gmail.com>
2023-02-08 16:51:27 +0300

    lesson 7

--- app/src/test/java/com/vk/testdrivendevelopment/game/model/QuestionUnitTest.kt
+++ app/src/test/java/com/vk/testdrivendevelopment/game/model/QuestionUnitTest.kt
@@ -1,33 +1,37 @@
 package com.vk.testdrivendevelopment.game.model
 
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 
 class QuestionUnitTest {
 
+    private lateinit var question: Question
+
+    @Before
+    fun setup(){
+        question = Question("CORRECT", "INCORRECT")
+    }
+
     @Test
     fun whenCreatingQuestion_shouldNotHaveAnsweredOption(){
-        val question = Question("CORRECT", "INCORRECT")
         Assert.assertNull(question.answeredOption)
     }
 
     @Test
     fun whenAnswering_shouldHaveAnsweredOption(){
-        val question = Question("CORRECT", "INCORRECT")
         question.answer("INCORRECT")
         Assert.assertEquals("INCORRECT", question.answeredOption)
     }
 
     @Test
     fun whenAnswering_withCorrectOption_shouldReturnTrue(){
-        val question = Question("CORRECT", "INCORRECT")
         val result = question.answer("CORRECT")
         Assert.assertTrue(result)
     }
 
     @Test
     fun whenAnswering_withIncorrectOption_shouldReturnFalse(){
-        val question = Question("CORRECT", "INCORRECT")
         val result = question.answer("INCORRECT")
         Assert.assertFalse(result)
     }
